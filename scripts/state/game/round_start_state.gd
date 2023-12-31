e76f78e0c358b0e9d5d4e881005696d673f7eede
bosettim <michael.bosetti@aviture.us.com>
2023-12-31 13:11:58 -0600

    make round health multiplier settable

--- scripts/state/game/round_start_state.gd
+++ scripts/state/game/round_start_state.gd
@@ -1,10 +1,12 @@
 extends "res://scripts/state/game/game_state.gd"
 
+@export var health_muliplier = 1.3
+
 func enter():
 	_start_round()
 
 func _start_round():
-	var round_health = (game.round + 1) * 1.3
+	var round_health = (game.round + 1) * health_muliplier
 	game._spawn_collidable_objects(round_health)
 	game.projectiles_count += 1
 	game.round += 1
